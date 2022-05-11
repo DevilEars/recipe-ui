bf6a4b552afed330685f7c2fb0597db95ba1dcc4
devilliers.neethling <DeVilliers.Neethling@standardbank.co.za>
2022-05-11 10:01:17 +0200

    Routing added

--- src/app/recipe-form/recipe-form.component.ts
+++ src/app/recipe-form/recipe-form.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-form',
   templateUrl: './recipe-form.component.html',
-  styleUrls: ['./recipe-form.component.css']
+  styleUrls: ['./recipe-form.component.css'],
 })
 export class RecipeFormComponent implements OnInit {
 
-  constructor() { }
+  model = { id: null, name: null, description: null};
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if(this.model.id){
+      // save new
+    }
+    else {
+      // update existing
+    }
+
+    this.router.navigate(['/']);
   }
-
 }
